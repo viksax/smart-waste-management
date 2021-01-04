1815d2ac3db09547278f0731b5a981c88b7387c0
viksax <30627941+viksax@users.noreply.github.com>
2021-01-04 20:38:34 +0530

    android app

---
+++ app/src/main/java/com/vik/smartwastemgmt/WhoAreYou.java
@@ -0,0 +1,13 @@
+package com.vik.smartwastemgmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhoAreYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_who_are_you);
+    }
+}
